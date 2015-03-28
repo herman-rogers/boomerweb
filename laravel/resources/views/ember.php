6eb26b72e43f2d9987be8262fd2666575a81edb6
Boomer <herman.rogers@gmail.com>
2015-03-28 21:48:55 +0000

    Adding new changes, switching from an ember cli project to a normal ember projct

---
+++ laravel/resources/views/ember.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>GweithdyEmber</title>
+  </head>
+  <body>
+      <h1> Gweithdy App </h1>
+      
+      <script src='../bower_components/jquery/dist/jquery.js'></script>
+      <script src='../bower_components/handlebars/handlebars.js'></script>
+      <script src='../bower_components/ember/ember.debug.js'></script>
+      <script src='../devgweithdyapp/public/app.js'></script>
+  </body>
+</html>
